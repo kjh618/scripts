6faa6ef5ee37493f5608fe49702fd8a51b223bd6
Jihoon Kim <kimjihoon618@gmail.com>
2026-02-25 16:48:26 +0900

    Add comment

--- install-nvim.sh
+++ install-nvim.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -ex
 
+# https://neovim.io/doc/install/#:~:text=Linux-,Pre%2Dbuilt%20archives
 curl --output-dir ~/Downloads -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim-linux-x86_64
 sudo tar -C /opt -xzf ~/Downloads/nvim-linux-x86_64.tar.gz
