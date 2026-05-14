7d2bbd91db8b461f139618b9c333242e9819b7f9
Jihoon Kim <kimjihoon618@gmail.com>
2026-05-14 20:24:36 +0900

    Fix ssh argument problem

--- remote-notify-send.sh
+++ remote-notify-send.sh
@@ -4,7 +4,8 @@ LOCAL_USER="$1"
 shift
 
 if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_CONNECTION" ] || [ -n "$SSH_TTY" ]; then
-    ssh -p 2222 "$LOCAL_USER@localhost" notify-send "$@"
+    ESCAPED_ARGS="$(printf '%q ' "$@")"
+    ssh -p 2222 "$LOCAL_USER@localhost" "notify-send $ESCAPED_ARGS"
 else
     notify-send "$@"
 fi
